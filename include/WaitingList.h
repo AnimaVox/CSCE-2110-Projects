be76866a9a480e28225c2dcd253c9e0974582cb7
TEX <TEXASDEAN2002@outlook.com>
2026-09-16 15:38:48 -0500

    Clean up initial prototype and fix include

--- include/WaitingList.h
+++ include/WaitingList.h
@@ -12,7 +12,7 @@ public:
 
     void RemoveStudent(); // Removes a student from the front of the queue
 
-    void DisplayWaiting(const Student student) const; // This should create a copy of the original queue so we don't accidentally destroy the actual queue displaying the data
+    void DisplayWaiting(const Student student); // This should create a copy of the original queue so we don't accidentally destroy the actual queue displaying the data
 
 private:
     queue<Student> students; // Queue containing student data
--- src/WaitingList.cpp
+++ src/WaitingList.cpp
@@ -1,4 +1,4 @@
-#include "CSCE-2110-Projects/include/WaitingList.h" // FIXME: Header include doesn't work
+#include "../include/WaitingList.h"
 
 void WaitingList::AddStudent(const Student& student) {
     students.push(student);
@@ -8,7 +8,12 @@ void WaitingList::RemoveStudent() {
     students.pop(student);
 }
 
-void WaitingList::DisplayWaiting(const Student student) const {
+void WaitingList::DisplayWaiting(const Student student) {
+    if (students.empty()) { // Check if the waiting list even has anything
+        cout << "The waiting list is empty!" << endl;
+        return;
+    }
+
     while (!students.empty()) {
         cout << students.front() << endl;
         students.pop();
